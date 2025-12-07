8c84707a0945450d18c39185cb23af7a40d01a83
roellongcop <30338518+roellongcop@users.noreply.github.com>
2025-12-07 23:42:28 +0800

    fix(*): add config

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js  (DO NOT commit this file)
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyATwTLLFfBdF6QKErEaaWHhShCHMhIM38k",
+  authDomain: "laundry-a0267.firebaseapp.com",
+  databaseURL: "https://laundry-a0267-default-rtdb.firebaseio.com",
+  projectId: "laundry-a0267",
+  storageBucket: "laundry-a0267.firebasestorage.app",
+  messagingSenderId: "272057038114",
+  appId: "1:272057038114:web:f0e1350416866fbe92617b",
+  measurementId: "G-VCXYYSESV8"
+}
